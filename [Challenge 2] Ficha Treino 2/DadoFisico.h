51506a2666fa077797925188ec3a9c92add0f1c5
Victor Watanabe <victor_rs_watanabe@hotmail.com>
2014-01-31 14:58:32 -0200

    Mudado o modelo de dados

---
+++ [Challenge 2] Ficha Treino 2/DadoFisico.h
@@ -0,0 +1,25 @@
+//
+//  DadoFisico.h
+//  [Challenge 2] Ficha Treino 2
+//
+//  Created by Victor Watanabe on 31/01/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DadoFisico : NSObject
+
+@property (nonatomic) float peso;
+
+@property (nonatomic) float braco;
+@property (nonatomic) float cintura;
+@property (nonatomic) float coxa;
+@property (nonatomic) float ombro;
+@property (nonatomic) float panturrilha;
+@property (nonatomic) float antebraco;
+@property (nonatomic) float quadris;
+@property (nonatomic) float porcentagemDeGordura;
+@property (nonatomic) float altura;
+
+@end
---
+++ [Challenge 2] Ficha Treino 2/DadoFisico.m
@@ -0,0 +1,13 @@
+//
+//  DadoFisico.m
+//  [Challenge 2] Ficha Treino 2
+//
+//  Created by Victor Watanabe on 31/01/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import "DadoFisico.h"
+
+@implementation DadoFisico
+
+@end
