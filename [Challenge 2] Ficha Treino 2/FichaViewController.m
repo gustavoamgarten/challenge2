8bb5ec4463cc190e704c784138bca38427b93595
gustavoamgarten <guamgarten@gmail.com>
2014-02-05 14:52:47 -0200

    Dados puxados da ficha: OK. Falta treinos.

--- [Challenge 2] Ficha Treino 2/FichaViewController.m
+++ [Challenge 2] Ficha Treino 2/FichaViewController.m
@@ -7,10 +7,20 @@
 //
 
 #import "FichaViewController.h"
+#import "Pessoa.h"
+#import "DataStorage.h"
+#import "Ficha.h"
+#import "Treinos.h"
 
 @interface FichaViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UILabel *dataLabel;
+@property (weak, nonatomic) IBOutlet UILabel *frequenciaLabel;
+@property (weak, nonatomic) IBOutlet UILabel *intervaloLabel;
+@property (weak, nonatomic) IBOutlet UILabel *periodoLabel;
+@property (weak, nonatomic) IBOutlet UILabel *periodoTipo;
+@property (weak, nonatomic) IBOutlet UILabel *objetivoLabel;
 
 @end
 
@@ -31,6 +41,56 @@
 	// Do any additional setup after loading the view.
     
     self.automaticallyAdjustsScrollViewInsets = NO; //Prevents top space navigation controller puts in CollectionView.
+    
+    //Ajuste de dados da ficha
+    [[DataStorage sharedRepository] reloadData];
+    NSArray *pessoas = [[DataStorage sharedRepository] getPessoas];
+    Pessoa *usuario = pessoas[0];
+    
+    NSArray* fichas = [usuario getFichas];
+    Ficha *ficha = fichas[0];
+    
+    //Data
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"dd/MM/yyyy";
+    self.dataLabel.text = [dateFormatter stringFromDate:ficha.dataDaCriacao];
+    
+    //Frequencia
+    self.frequenciaLabel.text = [NSString stringWithFormat:@"%@", ficha.frequencia];
+    
+    //Intervalo
+    self.intervaloLabel.text = [NSString stringWithFormat:@"%@", ficha.intervalo];
+    
+    //Periodo
+    NSInteger periodoTipo = [ficha.periodoTipo integerValue];
+    
+    if (periodoTipo == 0) {
+        self.periodoTipo.text = [NSString stringWithFormat:@"(semanas)"];
+    } else {
+        self.periodoTipo.text = [NSString stringWithFormat:@"(meses)"];
+    }
+    
+    self.periodoLabel.text = [NSString stringWithFormat:@"%@", ficha.periodoQuantidade];
+    
+    //Objetivo
+    NSInteger objetivo = [ficha.objetivo integerValue];
+    switch (objetivo) {
+        case 0:
+            self.objetivoLabel.text = [NSString stringWithFormat:@"Adaptação"];
+            break;
+        case 1:
+            self.objetivoLabel.text = [NSString stringWithFormat:@"Hipertrofia"];
+            break;
+        case 2:
+            self.objetivoLabel.text = [NSString stringWithFormat:@"Força"];
+            break;
+        case 3:
+            self.objetivoLabel.text = [NSString stringWithFormat:@"Definição"];
+            break;
+        default:
+            break;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -63,14 +123,12 @@
     return CGSizeMake(70, 70); //Return the size of the cell.
 }
 
-//#pragma mark - UIPickerViewDataSource
-//
-//- (NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-//    return 3;
-//}
-//
-//- (NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-//    return 1;
-//}
+- (IBAction)editButton:(UIButton *)sender {
+    NSLog(@"Cliquei no botao de Editar");
+}
+
+- (IBAction)novoTreinoButton:(UIButton *)sender {
+    NSLog(@"Cliquei no botao de Novo Treino");
+}
 
 @end
