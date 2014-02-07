2c1900efa4acf5746fa1baa9ea6a2bf7172b6593
Victor Watanabe <victor_rs_watanabe@hotmail.com>
2014-02-07 11:40:48 -0200

    Revert a63007e..b4b8168
    This rolls back to commit a63007e4efc96f8092dc33d0da83df5b39a19331.

--- [Challenge 2] Ficha Treino 2/Ficha.m
+++ [Challenge 2] Ficha Treino 2/Ficha.m
@@ -22,11 +22,13 @@
 @dynamic listaDeTreinos;
 @dynamic pessoa;
 
+#pragma mark - retornar objetos relacionados (Treinos)
 -(NSArray*)getListaTreinos
 {
     return [self.listaDeTreinos allObjects];
 }
 
+#pragma mark - Adicionar treino relacionado
 -(BOOL)addTreino:(NSString*)nome
 {
     
@@ -38,6 +40,112 @@
     
     NSError *error = nil;
     if (![fichaDeTreino.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+#pragma mark - Métodos para editar valores da ficha
+#pragma mark - métodos para editar individualmente os valores
+
+//métodos para editar individualmente os valores da ficha
+-(BOOL)editFrequencia:(int)frequencia
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.frequencia = [NSNumber numberWithInt:frequencia];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+-(BOOL)editIntervalo:(int)intervalo
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.intervalo = [NSNumber numberWithInt:intervalo];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+-(BOOL)editObjetivo:(int)objetivo
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.objetivo = [NSNumber numberWithInt:objetivo];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+-(BOOL)editPeriodoQuantidade:(int)periodoQuantidade
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.periodoQuantidade = [NSNumber numberWithInt:periodoQuantidade];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+-(BOOL)editPeriodoTipo:(int)periodoTipo
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.periodoTipo = [NSNumber numberWithInt:periodoTipo];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
+    {
+        return NO;
+    }
+    
+    [[DataStorage sharedRepository]reloadData];
+    return YES;
+}
+
+#pragma mark - métodos para editar todos os valores
+//métodos para editar todos os valores da ficha
+-(BOOL)editFichaFrequencia:(int)frequencia editIntervalo:(int)intervalo editObjetivo:(int)objetivo editPeriodoQuantidade:(int)periodoQuantidade editPeriodoTipo:(int)periodoTipo
+{
+    Ficha *fichaDePessoa = self;
+    
+    fichaDePessoa.frequencia = [NSNumber numberWithInt:frequencia];
+    fichaDePessoa.intervalo = [NSNumber numberWithInt:intervalo];
+    fichaDePessoa.objetivo = [NSNumber numberWithInt:objetivo];
+    fichaDePessoa.periodoQuantidade = [NSNumber numberWithInt:periodoQuantidade];
+    fichaDePessoa.periodoTipo = [NSNumber numberWithInt:periodoTipo];
+    
+    NSError *error = nil;
+    if (![fichaDePessoa.managedObjectContext save:&error])
     {
         return NO;
     }
