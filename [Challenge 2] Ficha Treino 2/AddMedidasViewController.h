28a5617fdf7012c56e7eba6a4e2e6e488540758c
Victor Watanabe <victor_rs_watanabe@hotmail.com>
2014-02-07 15:21:36 -0200

    Adicionado Classe View Controller para adição de medidas
    acima

---
+++ [Challenge 2] Ficha Treino 2/AddMedidasViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddMedidasViewController.h
+//  [Challenge 2] Ficha Treino 2
+//
+//  Created by Victor Watanabe on 07/02/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddMedidasViewController : UIViewController
+
+@end
---
+++ [Challenge 2] Ficha Treino 2/AddMedidasViewController.m
@@ -0,0 +1,39 @@
+//
+//  AddMedidasViewController.m
+//  [Challenge 2] Ficha Treino 2
+//
+//  Created by Victor Watanabe on 07/02/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import "AddMedidasViewController.h"
+
+@interface AddMedidasViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *TextField;
+@end
+
+@implementation AddMedidasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
